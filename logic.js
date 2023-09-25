dfe520beb6691fc3bec4962feca81029fb1ac181
Niyati Gupta <108604796+Elysian4@users.noreply.github.com>
2023-09-25 19:48:41 +0530

    Add files via upload

---
+++ logic.js
@@ -0,0 +1,143 @@
+//Create references to the dropdown's
+const yearSelect = document.getElementById("year");
+const monthSelect = document.getElementById("month");
+const daySelect = document.getElementById("day");
+
+const months = ['January', 'February', 'March', 'April', 
+'May', 'June', 'July', 'August', 'September', 'October',
+'November', 'December'];
+
+//Months are always the same
+(function populateMonths(){
+    for(let i = 0; i < months.length; i++){
+        const option = document.createElement('option');
+        option.textContent = months[i];
+        monthSelect.appendChild(option);
+    }
+    monthSelect.value = "January";
+})();
+
+let previousDay;
+
+function populateDays(month){
+    //Delete all of the children of the day dropdown
+    //if they do exist
+    while(daySelect.firstChild){
+        daySelect.removeChild(daySelect.firstChild);
+    }
+    //Holds the number of days in the month
+    let dayNum;
+    //Get the current year
+    let year = yearSelect.value;
+
+    if(month === 'January' || month === 'March' || 
+    month === 'May' || month === 'July' || month === 'August' 
+    || month === 'October' || month === 'December') {
+        dayNum = 31;
+    } else if(month === 'April' || month === 'June' 
+    || month === 'September' || month === 'November') {
+        dayNum = 30;
+    }else{
+        //Check for a leap year
+        if(new Date(year, 1, 29).getMonth() === 1){
+            dayNum = 29;
+        }else{
+            dayNum = 28;
+        }
+    }
+    //Insert the correct days into the day <select>
+    for(let i = 1; i <= dayNum; i++){
+        const option = document.createElement("option");
+        option.textContent = i;
+        daySelect.appendChild(option);
+    }
+    if(previousDay){
+        daySelect.value = previousDay;
+        if(daySelect.value === ""){
+            daySelect.value = previousDay - 1;
+        }
+        if(daySelect.value === ""){
+            daySelect.value = previousDay - 2;
+        }
+        if(daySelect.value === ""){
+            daySelect.value = previousDay - 3;
+        }
+    }
+}
+
+function populateYears(){
+    //Get the current year as a number
+    let year = new Date().getFullYear();
+    //Make the previous 100 years be an option
+    for(let i = 0; i < 101; i++){
+        const option = document.createElement("option");
+        option.textContent = year - i;
+        yearSelect.appendChild(option);
+    }
+}
+
+populateDays(monthSelect.value);
+populateYears();
+
+yearSelect.onchange = function() {
+    populateDays(monthSelect.value);
+}
+monthSelect.onchange = function() {
+    populateDays(monthSelect.value);
+}
+daySelect.onchange = function() {
+    previousDay = daySelect.value;
+}
+
+
+
+
+// There are many ways to pick a DOM node; here we get the form itself and the email
+// input box, as well as the span element into which we will place the error message.
+const form = document.querySelector("form");
+const email = document.getElementById("mail");
+const emailError = document.querySelector("#mail + span.error");
+
+email.addEventListener("input", (event) => {
+  // Each time the user types something, we check if the
+  // form fields are valid.
+
+  if (email.validity.valid) {
+    // In case there is an error message visible, if the field
+    // is valid, we remove the error message.
+    emailError.textContent = ""; // Reset the content of the message
+    emailError.className = "error"; // Reset the visual state of the message
+  } else {
+    // If there is still an error, show the correct error
+    showError();
+  }
+});
+
+form.addEventListener("Submit", (event) => {
+  // if the email field is valid, we let the form submit
+  if (!email.validity.valid) {
+    // If it isn't, we display an appropriate error message
+    showError();
+    // Then we prevent the form from being sent by canceling the event
+    event.preventDefault();
+  }
+});
+
+function showError() {
+  if (email.validity.valueMissing) {
+    // If the field is empty,
+    // display the following error message.
+    emailError.textContent = "You need to enter an email address.";
+  } else if (email.validity.typeMismatch) {
+    // If the field doesn't contain an email address,
+    // display the following error message.
+    emailError.textContent = "Entered value needs to be an email address.";
+  } else if (email.validity.tooShort) {
+    // If the data is too short,
+    // display the following error message.
+    emailError.textContent = `Email should be at least ${email.minLength} characters; you entered ${email.value.length}.`;
+  }
+
+  // Set the styling appropriately
+  emailError.className = "error active";
+}
